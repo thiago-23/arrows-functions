248cf352b2e7bdf7af9be9a26a8553fb00976fef
Thiago Alves <thiagosobral23@gmail.com>
2024-03-16 12:47:08 +0000

    Initial commit

--- arrow-functions.js
+++ arrow-functions.js
@@ -4,11 +4,40 @@
  */
 
 // Vanilla JavaScript Function
+// function addTwoNumbers(a, b) {
+//     //code block
+//     return a + b;
+// }
+// let sum = addTwoNumbers(3, 5);
+// console.log(sum);
 
 // Arrow Function With Parameters
+const addTwoNumbers = (a, b) => {
+    //code block
+    return a + b;
+}
+let sum = addTwoNumbers(3, 5);
+console.log(sum);
 
 // Single Line Arrow Function With Parameters
+// const addTwoNumbers2 = (a, b) => (a + b); also valid
+const addTwoNumbers2 = (a, b) => a + b; 
+let sum2 = addTwoNumbers2(6, 4);
+console.log(sum2);
+
 
 // Implicit Returns
+const saySomething = message => console.log(message);
+saySomething('Hello there!!')
+
+const sayHello = () => console.log('Hello');
+sayHello();
 
 // Returning Multiple Lines
+
+const returnMultiplrLines = () => (
+    `<p>
+        This is a multiple string!
+    </p>`
+)
+console.log(returnMultiplrLines());
